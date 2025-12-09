a8083e152aa48fe16dfed25a13909c14b5cf0d7f
Ketan <01fe24bcs151@kletech.ac.in>
2025-12-09 19:25:21 +0530

    Update and rename roads network.cpp to p1_mst_roads.cpp

--- p1_mst_roads.cpp
+++ p1_mst_roads.cpp
@@ -15,13 +15,13 @@ public:
         for (int i = 0; i < n; i++) parent[i] = i;
     }
     int findSet(int v) {
-        if (v == parent[v]) return v;
-        return parent[v] = findSet(parent[v]);
+        return (v == parent[v]) ? v : parent[v] = findSet(parent[v]);
     }
     bool unionSet(int a, int b) {
-        a = findSet(a); b = findSet(b);
+        a = findSet(a);
+        b = findSet(b);
         if (a == b) return false;
-        if (rnk[a] < rnk[b]) swap(a, b);
+        if (rnk[a] < rnk[b]) swap(a,b);
         parent[b] = a;
         if (rnk[a] == rnk[b]) rnk[a]++;
         return true;
@@ -35,7 +35,9 @@ struct MSTResult {
 
 MSTResult kruskalMST(int V, vector<Edge> edges) {
     MSTResult res;
-    sort(edges.begin(), edges.end(), [](auto &a, auto &b) { return a.w < b.w; });
+    sort(edges.begin(), edges.end(), [](auto &a, auto &b) {
+        return a.w < b.w;
+    });
     DSU dsu(V);
 
     for (auto &e : edges) {
@@ -52,53 +54,44 @@ MSTResult primMST(int V, const vector<vector<pair<int,int>>> &adj) {
     MSTResult res;
     vector<bool> inMST(V, false);
     vector<int> key(V, INT_MAX), parent(V, -1);
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+    priority_queue<pair<int,int>, vector<pair<int,int>>,
+                   greater<pair<int,int>>> pq;
 
     key[0] = 0;
-    pq.push({0, 0});
+    pq.push({0,0});
 
     while (!pq.empty()) {
-        auto [curW, u] = pq.top(); pq.pop();
+        auto [w,u] = pq.top(); pq.pop();
         if (inMST[u]) continue;
         inMST[u] = true;
-        res.totalCost += curW;
-
+        res.totalCost += w;
         if (parent[u] != -1)
-            res.edges.push_back({parent[u], u, curW});
+            res.edges.push_back({parent[u], u, w});
 
-        for (auto &nbr : adj[u]) {
-            int v = nbr.first, w = nbr.second;
-            if (!inMST[v] && w < key[v]) {
-                key[v] = w;
+        for (auto &[v,wt] : adj[u]) {
+            if (!inMST[v] && wt < key[v]) {
+                key[v] = wt;
                 parent[v] = u;
-                pq.push({w, v});
+                pq.push({wt,v});
             }
         }
     }
     return res;
 }
 
-void printMST(string title, MSTResult &mst) {
-    cout << "\n" << title << ":\n";
-    cout << "Total Cost: " << mst.totalCost << "\n";
-    for (auto &e : mst.edges)
-        cout << e.u << " -- " << e.v << "  (Cost " << e.w << ")\n";
-}
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     ifstream fin("road_network.txt");
     if (!fin.is_open()) {
-        cout << "Input file road_network.txt not found!\n";
+        cout << "road_network.txt not found\n";
         return 0;
     }
 
     int V, E;
     fin >> V >> E;
+
     vector<Edge> edges(E);
-    for (int i = 0; i < E; i++) fin >> edges[i].u >> edges[i].v >> edges[i].w;
+    for (int i = 0; i < E; i++)
+        fin >> edges[i].u >> edges[i].v >> edges[i].w;
     fin.close();
 
     vector<vector<pair<int,int>>> adj(V);
@@ -110,14 +103,21 @@ int main() {
     MSTResult K = kruskalMST(V, edges);
     MSTResult P = primMST(V, adj);
 
-    printMST("Kruskal MST", K);
-    printMST("Prim MST", P);
+    cout << "Kruskal MST\n";
+    cout << "Cost: " << K.totalCost << "\n";
+    for (auto &e : K.edges)
+        cout << e.u << " -- " << e.v << " (" << e.w << ")\n";
 
-    cout << "\nComparison Result:\n";
+    cout << "\nPrim MST\n";
+    cout << "Cost: " << P.totalCost << "\n";
+    for (auto &e : P.edges)
+        cout << e.u << " -- " << e.v << " (" << e.w << ")\n";
+
+    cout << "\n";
     if (K.totalCost == P.totalCost)
-        cout << "✔ Both MST costs match. Implementation correct.\n";
+        cout << "Costs Match ✔\n";
     else
-        cout << "⚠ Cost mismatch. Check graph data.\n";
+        cout << "Cost Mismatch ⚠\n";
 
     return 0;
 }
